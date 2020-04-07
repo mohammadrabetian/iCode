46e36be097ffaf6eed3f28763981024cf3d8fea5
mohammadrabetian <tommyrabetian@gmail.com>
2020-04-08 01:56:16 +0430

    file model

--- base/models.py
+++ base/models.py
@@ -8,3 +8,14 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return "user_{0}/{1}".format(instance.user.id, filename)
+
+
+class FileModel(BaseModel):
+    file = models.FileField(upload_to=user_directory_path)
+    file_type = models.CharField(max_length=20, null=True, blank=True)
+    alt = models.CharField(max_length=20, null=True, blank=True)
