0d8454acecec91ae712ec8481a7d2c2e7a3c7127
Sorkhemiri <mahdi.s@arianatech.ir>
2020-04-15 09:31:19 +0430

    question and exam model added to admin panel

--- exam/admin.py
+++ exam/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Exam, Question
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "is_deleted", "created_at", "updated_at")
+    search_fields = ["id", "title", "about"]
+    date_hierarchy = "updated_at"
+
+@admin.register(Question)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ("id", "exam", "answer_type", "created_at", "updated_at")
+    search_fields = ["id", "question_text"]
+    date_hierarchy = "updated_at"
+
