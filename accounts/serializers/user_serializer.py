76a8fcebd6ee895886fc4b16330d240d6810eb2c
Sorkhemiri <mahdi.s@arianatech.ir>
2020-04-16 22:38:38 +0430

    later

--- accounts/serializers/user_serializer.py
+++ accounts/serializers/user_serializer.py
@@ -5,12 +5,15 @@ from accounts.models import User
 
 
 class CreateUserSerializer(serializers.Serializer):
+    id = serializers.CharField(read_only=True)
     username = serializers.CharField()
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True, required=True)
     first_name = serializers.CharField()
     last_name = serializers.CharField()
-    
+    full_name = serializers.SerializerMethodField("get_full_name", read_only=True)
+    last_login = serializers.DateTimeField(read_only=True)
+    date_joined = serializers.DateTimeField(read_only=True)
     def save(self):
         self.validated_data['password'] = make_password(self.validated_data.get("password"))
         return super(CreateUserSerializer, self).save()
@@ -18,3 +21,10 @@ class CreateUserSerializer(serializers.Serializer):
     def create(self, validated_data):
         return User.objects.create(**validated_data)
 
+    def get_full_name(self, obj):
+        if obj.first_name and obj.last_name:
+            return obj.first_name + " " + obj.last_name
+        elif obj.first_name or obj.last_name:
+            return obj.first_name if obj.first_name else obj.last_login
+        else:
+            return None
