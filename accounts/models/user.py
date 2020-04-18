ae01463c4c3ada26b37afd695c7a190697d4fcb2
mohammadrabetian <tommyrabetian@gmail.com>
2020-04-18 16:54:51 +0430

    some fields for user model

--- accounts/models/user.py
+++ accounts/models/user.py
@@ -3,7 +3,26 @@ from django.db import models
 
 
 class User(AbstractUser):
+    # genders
+    MALE = "male"
+    FEMALE = "female"
+    OTHER = "Other"
+    GENDERS = (
+        (MALE, "Male"),
+        (FEMALE, "Female"),
+        (OTHER, "Other"),
+    )
+
     phone_number = models.CharField(max_length=50, blank=True, null=True)
+    gender = models.CharField(max_length=100, null=True, blank=True, choices=GENDERS)
+    birthdate = models.DateField(null=True, blank=True)
+    job_title = models.ForeignKey(
+        "jobs.JobTitle", on_delete=models.SET_NULL, null=True, blank=True
+    )  # TODO: jobs title model
+    city = models.ForeignKey(
+        "accounts.City", on_delete=models.SET_NULL, null=True, blank=True
+    )  # TODO: city model
+    email_confirmed = models.BooleanField(default=False)
 
     class Meta:
         app_label = "accounts"
