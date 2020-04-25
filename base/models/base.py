d827c63a97f452b915dc535fee563c72f69b824c
mohammadrabetian <tommyrabetian@gmail.com>
2020-04-25 17:03:52 +0430

    enhance base model

--- base/models/base.py
+++ base/models/base.py
@@ -1,11 +1,21 @@
+import uuid
+from datetime import datetime
+
 from django.db import models
 
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
     is_deleted = models.BooleanField(default=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     class Meta:
         app_label = "base"
         abstract = True
+
+    def soft_delete(self):
+        self.deleted_at = datetime.utcnow()
+        self.is_deleted = True
+        self.save()
