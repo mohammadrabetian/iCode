22e6c07dcfd4bd1d599b3c42bd13c60922a5845f
mohammadrabetian <tommyrabetian@gmail.com>
2020-04-26 19:21:48 +0430

    relocate get env helper to base app

--- icode/settings.py
+++ icode/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-from .helpers import get_env_variable
+from base.helpers import get_env_variable
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
