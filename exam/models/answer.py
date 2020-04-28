70e1f03258670cf76abfae8e6afed5475a41f4bc
mohammadrabetian <tommyrabetian@gmail.com>
2020-04-28 19:00:46 +0430

    Satisfactory rate for answers to text based questions

--- exam/models/answer.py
+++ exam/models/answer.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from base.models import BaseModel
 
@@ -12,7 +13,12 @@ class Answer(BaseModel):
     )
     answer_text = models.TextField(null=True, blank=True)
     answer_choice = models.CharField(max_length=100, null=True, blank=True)
+    # for choice field questions
     is_valid = models.NullBooleanField()
+    # for text field questions
+    satisfactory_rate = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)], default=0
+    )
 
     class Meta:
         app_label = "exam"
